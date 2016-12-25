426727a959c18dcfb78776f6476e668bd0a13078
Milan Kacálek <kacis.m@gmail.com>
2016-12-25 11:13:35 +0000

    Change for build in Travis CI.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM kacis/docker-rpi-perl-ssl:5.24.0
 
 MAINTAINER Milan Kacalek <kacis.m@gmail.com>
 
-RUN mkdir -p /var/cache/ddclient/
-
 ADD ddclient /usr/bin/
 ADD run.sh /usr/bin/
 
--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ mkdir -p /tmp/
 rm -f /tmp/ddclient-3.8.2.tar.bz2
 wget "http://downloads.sourceforge.net/project/ddclient/ddclient/ddclient-3.8.2/ddclient-3.8.2.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fddclient%2F&ts=1408222484&use_mirror=softlayer-dal" -O /tmp/ddclient-3.8.2.tar.bz2
 tar -xvf /tmp/ddclient-3.8.2.tar.bz2
-cp ddclient-3.8.2/ddclient ./ 
+cp ddclient-3.8.2/ddclient ./
+mkdir -p cache/ddclient/
